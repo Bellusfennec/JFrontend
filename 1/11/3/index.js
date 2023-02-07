a7b952fdeda63b1486dd9701cb943e5fe5f5a3d1
bellusfennec <bellusfennec@gmail.com>
2023-02-07 16:32:38 +0300

    Решение  1-11-2, Промежуточный 1-11-3

---
+++ 1/11/3/index.js
@@ -0,0 +1,9 @@
+const addDays = (date, days) => {
+  return convertMsToDays(nextBirthdayDate) - convertMsToDays(new Date());
+};
+
+function convertMsToDays(ms) {
+  return Math.round(ms / 1000 / 60 / 60 / 24);
+}
+
+getDaysBeforeBirthday(new Date("November 11, 2023"))
