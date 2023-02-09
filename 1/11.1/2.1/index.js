34262a89ad713916fbf98301fb6ae76111190ee2
bellusfennec <bellusfennec@gmail.com>
2023-02-09 16:25:11 +0300

    Промежуточное решение

---
+++ 1/11.1/2.1/index.js
@@ -0,0 +1,35 @@
+const peopleWithVisa = [
+  {
+    firstName: "Stasia",
+    lastName: "Ward",
+    criminalRecord: true,
+    passportExpiration: "19.06.2040",
+  },
+  {
+    firstName: "Elliot",
+    lastName: "Baker",
+    criminalRecord: false,
+    passportExpiration: "04.06.2041",
+  },
+  {
+    firstName: "Leighann",
+    lastName: "Scott",
+    criminalRecord: true,
+    passportExpiration: "31.07.2039",
+  },
+  {
+    firstName: "Nick",
+    lastName: "Pop",
+    criminalRecord: false,
+    passportExpiration: "31.12.2010",
+  },
+];
+
+function allowVisa(peaple) {
+  return peaple.filter(human => {
+    return human.criminalRecord
+  })
+}
+
+const result = allowVisa(peopleWithVisa);
+console.log("result", result);
