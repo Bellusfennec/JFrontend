4feca6aaf50867b0ed6368c9b160f326c6350048
Bellusfennec <bellusfennec@gmail.com>
2023-02-16 20:56:25 +0300

    Решение 1-12-3

--- 1/12/3/index.js
+++ 1/12/3/index.js
@@ -1,24 +1,25 @@
 const footballer = {
-  fullName: 'Cristiano Ronaldo',
-  attack: () => {
-     console.log(`${this.fullName} сейчас с мячом и начинает атаку!`);
+  fullName: "Cristiano Ronaldo",
+  attack: function() {
+    console.log(`${this.fullName} сейчас с мячом и начинает атаку!`);
   },
   scoreGoal(sound) {
-     console.log(`${this.fullName} забил гол! Вот это да!`);
-     this.celebrate(sound);
+    console.log(`${this.fullName} забил гол! Вот это да!`);
+    this.celebrate(sound);
   },
   celebrate(sound) {
-     console.log(sound);
+    console.log(sound);
   },
-  goToSubstitution: function(newPlayer) {
-     console.log(`${this.fullName} уходит на замену. На поле выходит ${newPlayer}`);
-  }
+  goToSubstitution: function (newPlayer) {
+    console.log(
+      `${this.fullName} уходит на замену. На поле выходит ${newPlayer}`
+    );
+  },
 };
 
 const attack = footballer.attack.bind(footballer);
-console.log(attack);
-const score = footballer.scoreGoal.call(footballer);
-const substitute = footballer.goToSubstitution.apply(footballer)
-attack();
-score('Сиииии');
-substitute('Paulo Dibala');
+const score = footballer.scoreGoal
+const substitute = footballer.goToSubstitution
+attack()
+score.call(footballer, "Сиииии");
+substitute.apply(footballer, ["Paulo Dibala"]);
