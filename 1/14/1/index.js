25d6e9c4be1323d400f89b590a6d1f1b097e011f
bellusfennec <bellusfennec@gmail.com>
2023-02-20 16:13:34 +0300

    Промежуточное

---
+++ 1/14/1/index.js
@@ -0,0 +1,4 @@
+const body = document.querySelector("body");
+// body.innerHTML = '<form class="create-user-form"><label> Имя  <input type="text" name="userName" placeholder="Введите ваше имя" /></label><label>  Пароль  <input type="password" name="password" placeholder="Придумайте Пароль"  /></label><button type="submit">Подтвердить</button></form>';
+console.log(body);
+const from = body.createElement("form");
