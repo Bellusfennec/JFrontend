4dd5cf3245546ba7038645b7eedd8ac983515dd8
Bellusfennec <bellusfennec@gmail.com>
2023-02-21 19:00:41 +0300

    Доделка 1-15-1

--- 1/15/1/index.js
+++ 1/15/1/index.js
@@ -1,9 +1,12 @@
+const tasks = []
+
 const form = document.querySelector('.create-task-block')
-const tasks = document.querySelector('.tasks-list')
+const tasksList = document.querySelector('.tasks-list')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   const text = event.target[0].value
   const id = new Date().getTime()
-  tasks.insertAdjacentHTML('beforeend', `<p id='${id}'>${text}</p>`)
+  tasksList.insertAdjacentHTML('beforeend', `<p id='${id}'>${text}</p>`)
+  tasks.push({id, text})
 })
