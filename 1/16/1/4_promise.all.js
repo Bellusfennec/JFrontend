b8c8ddfbaa271f5f338cb0d6fddbe154ad1888ff
bellusfennec <bellusfennec@gmail.com>
2023-03-07 10:31:38 +0300

    Решение 1-16-1

--- 1/16/1/4_promise.all.js
+++ 1/16/1/4_promise.all.js
@@ -1,1 +1,14 @@
-fetch("https://jsonplaceholder.typicode.com/users").then();
+const dataContainer = document.querySelector("#data-container");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Ошибка запроса");
+    }
+    return response.json()
+  })
+  .then((data) => data.map(item => {
+
+    dataContainer.insertAdjacentHTML('beforeend', `<li><a href="#">${item.username}</a></li>`)
+  }))
+  .catch((error) => console.error(error));
