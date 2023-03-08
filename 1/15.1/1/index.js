877890587b95a7ecbc9443a15b2dee14a13f6d4e
Bellusfennec <bellusfennec@gmail.com>
2023-03-08 20:21:29 +0300

    промежуточное

--- 1/15.1/1/index.js
+++ 1/15.1/1/index.js
@@ -1,50 +1,63 @@
 class CustomSelect {
-  #id
-  #option
-  #currentSelectedOption
+  #id;
+  #option;
+  #currentSelectedOption;
   constructor(id, options) {
     this.#id = id;
-    this.#option = options
-    this.#currentSelectedOption = null
+    this.#option = options;
+    this.#currentSelectedOption = null;
   }
   render(container) {
     console.log(this);
-    
+
     container.insertAdjacentHTML(
       "beforeend",
-      `<div class="select-dropdown select-dropdown--123">
-    <button class="select-dropdown__button select-dropdown__button--123"> 
-       <span class="select-dropdown__text select-dropdown__text--123">Выберите элемент</span>
-    </button>
-   <ul class="select-dropdown__list select-dropdown__list--123"> 
-      <li class="select-dropdown__list-item" data-value="1">JavaScript</li>
-      <li class="select-dropdown__list-item" data-value="2">NodeJS</li>
-      <li class="select-dropdown__list-item" data-value="3">ReactJS</li>
-      <li class="select-dropdown__list-item" data-value="4">HTML</li>
-      <li class="select-dropdown__list-item" data-value="5">CSS</li> 
-   </ul> 
- </div>`
+      `
+      <div class="select-dropdown select-dropdown--${this.#id}">
+        <button class="select-dropdown__button select-dropdown__button--${
+          this.#id
+        }"> 
+          <span class="select-dropdown__text select-dropdown__text--${
+            this.#id
+          }">Выберите элемент</span>
+        </button>
+        <ul class="select-dropdown__list select-dropdown__list--${
+          this.#id
+        }"></ul> 
+      </div>
+      `
     );
-    console.log('selectedValue()', this.selectedValue);
+    const selectDropdown = document.querySelector(".select-dropdown__list");
+    this.#option.map((item) => {
+      selectDropdown.insertAdjacentHTML(
+        "beforeend",
+        `
+        <li class="select-dropdown__list-item" data-value="${item.value}">
+          ${item.text}
+        </li>
+        `
+      );
+    });
+    console.log("selectedValue()", this.selectedValue);
   }
   get selectedValue() {
-    const item = document.querySelectorAll('.select-dropdown__list-item')
+    const item = document.querySelectorAll(".select-dropdown__list-item");
     // item.addEventListener('click', (event) => {
     //   console.log(event);
     // })
     console.log(item);
-    return 'get'
+    return "get";
   }
 }
 
 const options = [
-  { value: 1, text: 'JavaScript' },
-  { value: 2, text: 'NodeJS' },
-  { value: 3, text: 'ReactJS' },
-  { value: 4, text: 'HTML' },
-  { value: 5, text: 'CSS' }
+  { value: 1, text: "JavaScript" },
+  { value: 2, text: "NodeJS" },
+  { value: 3, text: "ReactJS" },
+  { value: 4, text: "HTML" },
+  { value: 5, text: "CSS" },
 ];
 
-const customSelect = new CustomSelect('123', options);
-const mainContainer = document.querySelector('#container'); 
+const customSelect = new CustomSelect("123", options);
+const mainContainer = document.querySelector("#container");
 customSelect.render(mainContainer);
